c89d520f44216077b1601bb7a7a4faac454fa040
h4sski.programming <h4sski.programming@gmail.com>
2024-08-09 09:05:46 +0200

    calculator done, next step is add extra conditions and fulfill database

---
+++ dropdown_button.py
@@ -0,0 +1,16 @@
+from kivy.metrics import dp
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
+
+class Dropdown_Button(Button):
+    def __init__(self, list_of_choises: list, btn_text:str, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.text = btn_text
+        self.dropdown_btn = DropDown()
+        for choise in list_of_choises:
+            btn = Button(text=str(choise), size_hint=(None, None), size=(dp(150), dp(22)))
+            btn.bind(on_release=lambda btn: self.dropdown_btn.select(btn.text))
+            self.dropdown_btn.add_widget(btn)
+        self.bind(on_release=self.dropdown_btn.open)
+        self.dropdown_btn.bind(on_select=lambda instance, x: setattr(self, 'text', x))
